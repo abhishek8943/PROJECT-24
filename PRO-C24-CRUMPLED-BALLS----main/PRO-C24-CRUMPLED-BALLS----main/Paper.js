dd656c0a3f798c361c668bf885159715d23e84fd
abhishek8943 <82888129+abhishek8943@users.noreply.github.com>
2021-10-22 18:50:42 +0530

    Add files via upload

---
+++ PRO-C24-CRUMPLED-BALLS----main/PRO-C24-CRUMPLED-BALLS----main/Paper.js
@@ -0,0 +1,25 @@
+class Paper {
+    constructor(x, y, radius) {
+      var options = {
+          'restitution':0.8,
+          'friction':0.3,
+          'density':1.0
+      }
+      this.body = Bodies.circle(x, y, radius, options);
+      this.radius=2*radius;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      strokeWeight(4);
+        stroke("blue");
+      fill('pink');
+      ellipse(0, 0, this.radius, this.radius);
+      pop();
+    }
+  };
